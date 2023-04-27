6bef0f7f01807bb7810690530f2471b0f36c0bd2
Kiruthik Kumar <92621351+rith1x@users.noreply.github.com>
2023-04-27 12:18:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Define the 2D matrix
+matrix = np.array([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]])
+
+# Create a heatmap of the matrix
+plt.imshow(matrix, cmap='gray')
+plt.title('Matrix')
+plt.axis('off')
+st.pyplot()
+
+# Create input fields for the starting and ending points
+start_point = st.text_input('Enter the starting point (row, column):')
+end_point = st.text_input('Enter the ending point (row, column):')
+
+# Convert the input to integers
+start_point = tuple(map(int, start_point.split(',')))
+end_point = tuple(map(int, end_point.split(',')))
+
+# Implement Dijkstra's algorithm to find the shortest path
+G = nx.grid_2d_graph(matrix.shape[0], matrix.shape[1])
+for i in range(matrix.shape[0]):
+    for j in range(matrix.shape[1]):
+        if matrix[i, j] == 0:
+            G.remove_node((i, j))
+            
+shortest_path = nx.dijkstra_path(G, start_point, end_point)
+
+# Highlight the shortest path on the heatmap
+for i, j in shortest_path:
+    plt.gca().add_patch(plt.Rectangle((j-0.5, i-0.5), 1, 1, fill=False, edgecolor='blue', lw=3))
+    
+# Display the modified heatmap
+plt.imshow(matrix, cmap='gray')
+plt.title('Shortest Path')
+plt.axis('off')
+st.pyplot()
