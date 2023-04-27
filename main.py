9d5cd98aa1373c494ac475b76c1d2de3cb0f370d
Kiruthik Kumar <92621351+rith1x@users.noreply.github.com>
2023-04-27 12:27:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
+# from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
 
-# Define the 2D matrix
-matrix = np.array([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+matrix = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1],
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -21,37 +19,13 @@ matrix = np.array([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 [0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]])
+[0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]]
 
-# Create a heatmap of the matrix
-plt.imshow(matrix, cmap='gray')
-plt.title('Matrix')
-plt.axis('off')
-st.pyplot()
+grid = Grid(matrix=matrix)
+start = grid.node(0, 0)
+end = grid.node(5, 2)
+finder = AStarFinder()
 
-# Create input fields for the starting and ending points
-start_point = st.text_input('Enter the starting point (row, column):')
-end_point = st.text_input('Enter the ending point (row, column):')
+path, runs = finder.find_path(start, end, grid)
 
-# Convert the input to integers
-start_point = tuple(map(int, start_point.split(',')))
-end_point = tuple(map(int, end_point.split(',')))
-
-# Implement Dijkstra's algorithm to find the shortest path
-G = nx.grid_2d_graph(matrix.shape[0], matrix.shape[1])
-for i in range(matrix.shape[0]):
-    for j in range(matrix.shape[1]):
-        if matrix[i, j] == 0:
-            G.remove_node((i, j))
-            
-shortest_path = nx.dijkstra_path(G, start_point, end_point)
-
-# Highlight the shortest path on the heatmap
-for i, j in shortest_path:
-    plt.gca().add_patch(plt.Rectangle((j-0.5, i-0.5), 1, 1, fill=False, edgecolor='blue', lw=3))
-    
-# Display the modified heatmap
-plt.imshow(matrix, cmap='gray')
-plt.title('Shortest Path')
-plt.axis('off')
-st.pyplot()
+print(path)
